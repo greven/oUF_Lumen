cfc47de9633e5bdb74c3176bb05db00c75f0bae4
Nuno M <greven.pt@gmail.com>
2018-07-24 01:26:50 +0100

    Update oUF_Smooth with Heal Prediction

--- embeds/oUF_Smooth.lua
+++ embeds/oUF_Smooth.lua
@@ -4,14 +4,11 @@ assert(oUF, "<name> was unable to locate oUF install.")
 
 local smoothing = {}
 local function Smooth(self, value)
-	local _, max = self:GetMinMaxValues()
-	if value == self:GetValue() or (self._max and self._max ~= max) then
-		smoothing[self] = nil
-		self:SetValue_(value)
-	else
+	if value ~= self:GetValue() or value == 0 then
 		smoothing[self] = value
+	else
+		smoothing[self] = nil
 	end
-	self._max = max
 end
 
 local function SmoothBar(self, bar)
@@ -27,6 +24,20 @@ local function hook(frame)
 	if frame.Power and frame.Power.Smooth then
 		frame:SmoothBar(frame.Power)
 	end
+	if frame.HealthPrediction then
+		if (frame.HealthPrediction.myBar and frame.HealthPrediction.myBar.Smooth) then
+			frame:SmoothBar(frame.HealthPrediction.myBar)
+		end
+		if (frame.HealthPrediction.otherBar and frame.HealthPrediction.otherBar.Smooth) then
+			frame:SmoothBar(frame.HealthPrediction.otherBar)
+		end
+		if (frame.HealthPrediction.absorbBar and frame.HealthPrediction.absorbBar.Smooth) then
+			frame:SmoothBar(frame.HealthPrediction.absorbBar)
+		end
+		if (frame.HealthPrediction.healAbsorbBar and frame.HealthPrediction.healAbsorbBar.Smooth) then
+			frame:SmoothBar(frame.HealthPrediction.healAbsorbBar)
+		end
+	end
 end
 
 
@@ -34,18 +45,27 @@ for i, frame in ipairs(oUF.objects) do hook(frame) end
 oUF:RegisterInitCallback(hook)
 
 
-local f, min, max = CreateFrame('Frame'), math.min, math.max 
-f:SetScript('OnUpdate', function()
+local f, min, max = CreateFrame("Frame"), math.min, math.max
+f:SetScript("OnUpdate", function()
 	local limit = 30/GetFramerate()
 	for bar, value in pairs(smoothing) do
 		local cur = bar:GetValue()
-		local new = cur + min((value-cur)/3, max(value-cur, limit))
+		local barmin, barmax = bar:GetMinMaxValues()
+		local new = cur + min((value-cur)/10, max(value-cur, limit))
 		if new ~= new then
-			-- Mad hax to prevent QNAN.
 			new = value
 		end
 		bar:SetValue_(new)
-		if cur == value or abs(new - value) < 2 then
+    if bar.Filling then
+      if barmax == 0 then
+        bar.Filling:SetHeight(0)
+        bar.Filling:SetTexCoord(0,1,1,1)
+      else
+        bar.Filling:SetHeight((new / barmax) * bar:GetWidth())
+        bar.Filling:SetTexCoord(0,1,  math.abs(new / barmax - 1),1)
+      end
+    end
+		if cur == value or abs(cur - value) < 2 then
 			bar:SetValue_(value)
 			smoothing[bar] = nil
 		end
