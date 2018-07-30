f4b095016829ee5edc607ccd49e1dc9d60e00a5b
Nuno M <greven.pt@gmail.com>
2018-07-30 10:47:33 +0100

    Add Fatigued to debuffs ignore list

--- config/debuffs.lua
+++ config/debuffs.lua
@@ -10,7 +10,9 @@ ns.debuffs = debuffs
 debuffs.list = {
   player = {
     [GetSpellInfo(57723) or "Exhaustion"] = true,
+    [GetSpellInfo(264689) or "Fatigued"] = true,
     [GetSpellInfo(95809) or "Insanity"] = true,
+    [GetSpellInfo(57724) or "Sated"] = true,
     [GetSpellInfo(80354) or "Temporal Displacement"] = true,
   },
   target = {
