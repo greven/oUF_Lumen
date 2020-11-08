60b4027d1c6fcdc0230f98e42decd7d54498793e
Nuno Freire <greven.pt@gmail.com>
2020-11-08 23:09:20 +0000

    Filters

--- config/AuraBars/Druid.lua
+++ config/AuraBars/Druid.lua
@@ -4,6 +4,10 @@ local filters = ns.filters
 
 filters.DRUID = {
   buffs = {
+    [1850 or "Dash"] = true,
+    [48518 or "Eclipse (Lunar)"] = true,
+    [48517 or "Eclipse (Solar)"] = true,
+    [192081 or "Iron Fur"] = true,
     [77764 or "Stampeding Roar"] = true,
     [191034 or "Starfall"] = true
   },
