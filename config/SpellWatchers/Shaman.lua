f878ece34b8022116ed5f74dd10cb2586294b20d
Nuno Freire <greven.pt@gmail.com>
2020-11-23 22:00:09 +0000

    Filter updates

--- config/SpellWatchers/Shaman.lua
+++ config/SpellWatchers/Shaman.lua
@@ -4,11 +4,19 @@ local watchers = ns.watchers
 
 watchers.SHAMAN = {
     [262] = { -- Elemental
+        [1] = {spellID = 188389}, -- Flame Shock
+        [2] = {spellID = 51505, auraID = 77762, glow = "button"}, -- Lava Burst / Lava Surge
+        [3] = {spellID = 8042, glow = "pixel"}, -- Earth Shock
+        [4] = {spellID = 191634}, -- Stormkeeper
+        [5] = {spellID = 198067}, -- Fire Elemental
 
     },
     [263] = { -- Enhancement
-        [1] = {spellID = 17364}, -- Stormstrike
-        [2] = {spellID = 60103} -- Lava Lash
+        [1] = {spellID = 17364, auraID = 201846, glow = "button"}, -- Stormstrike / Stormbring
+        [2] = {spellID = 60103}, -- Lava Lash
+        [3] = {spellID = 188389}, -- Flame Shock
+        [4] = {spellID = 187874}, -- Crash Lightning
+        [5] = {spellID = 188443, auraID = 344179, auraCount = 5, glow = "button"}, -- Chain Lightning / Maelstrom Weapon
     },
     [264] = { -- Restoration
 
--- config/SpellWatchers/Warrior.lua
+++ config/SpellWatchers/Warrior.lua
@@ -7,7 +7,11 @@ watchers.WARRIOR = {
 
     },
     [72] = { -- Fury
-
+        [1] = {spellID = 23881}, -- Bloodthirst
+        [2] = {spellID = 85288}, -- Raging Blow
+        [3] = {spellID = 184367, glow = "pixel"}, -- Rampage
+        [4] = {spellID = 5308, auraID = 280776, glow = "button"}, -- Execute / Sudden Death
+        [5] = {spellID = 1719} -- Recklessness
     },
     [73] = { -- Protection
 
