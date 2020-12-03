27716d273ea7b1df8f10b5f7594cb7277019eb26
Nuno Freire <greven.pt@gmail.com>
2020-12-03 00:32:36 +0000

    Update Filters

--- config/SpellWatchers/Priest.lua
+++ config/SpellWatchers/Priest.lua
@@ -7,8 +7,8 @@ watchers.PRIEST = {
         [1] = {spellID = 47540, auraID = 198069, glow = "pixel"}, -- Penance /
         [2] = {spellID = 194509}, -- Power Word: Radiance
         [3] = {spellID = 8092}, -- Mind Blast
-        [4] = {spellID = 214621}, -- Schism
-        [5] = {spellID = 129250} -- Power Word: Solace
+        [4] = {spellID = 129250}, -- Power Word: Solace
+        [5] = {spellID = 214621} -- Schism
     },
     [257] = { -- Holy
         [1] = {spellID = 2061, auraID = 114255, glow = "button"}, -- Flash Heal / Surge of Light
@@ -20,8 +20,8 @@ watchers.PRIEST = {
     [258] = { -- Shadow
         [1] = {spellID = 8092, auraID = 341207, glow = "button"}, -- Mind Blast / Dark Thought
         [2] = {spellID = 335467, glow = "pixel"}, -- Devouring Plague
-        [3] = {spellID = 32379}, -- Shadow Word: Death
-        [4] = {spellID = 263165}, -- Void Torrent
-        [5] = {spellID = 228260, auraID = 194249, altID = 205448, glow = nil} -- Void Eruption / Voidform / Void Bolt
+        [3] = {spellID = 228260, auraID = 194249, altID = 205448, glow = nil}, -- Void Eruption / Voidform / Void Bolt
+        [4] = {spellID = 32379}, -- Shadow Word: Death
+        [5] = {spellID = 263165}, -- Void Torrent
     }
 }
--- config/SpellWatchers/Warrior.lua
+++ config/SpellWatchers/Warrior.lua
@@ -10,8 +10,9 @@ watchers.WARRIOR = {
         [1] = {spellID = 23881}, -- Bloodthirst
         [2] = {spellID = 85288}, -- Raging Blow
         [3] = {spellID = 184367, glow = "pixel"}, -- Rampage
-        [4] = {spellID = 5308, auraID = 280776, glow = "button"}, -- Execute / Sudden Death
-        [5] = {spellID = 1719} -- Recklessness
+        [4] = {spellID = 315720, glow = "pixel"}, -- Onslaught
+        [5] = {spellID = 280772} -- Siegebreaker
+        -- [5] = {spellID = 5308, auraID = 280776, glow = "button"}, -- Execute / Sudden Death
     },
     [73] = { -- Protection
 
